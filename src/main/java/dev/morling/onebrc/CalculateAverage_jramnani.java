ea198e22f71d0802d755390e5dcde3d379f6729e
Jeff Ramnani <jramnani@8thlight.com>
2024-01-04 11:34:28 -0800

    Process stream in parallel
    Performance: 0m54.140s
    
    This is the first big speed up.  And kudos to Java's Collection API.
    Adding 1 line allowed me to run it in parallel and improve the
    performance by 68%.

--- src/main/java/dev/morling/onebrc/CalculateAverage_jramnani.java
+++ src/main/java/dev/morling/onebrc/CalculateAverage_jramnani.java
@@ -83,6 +83,7 @@ public class CalculateAverage_jramnani {
                 });
 
         Map<String, ResultRow> measurements = new TreeMap<>(Files.lines(Paths.get(FILE))
+                .parallel()
                 .map(l -> new Measurement(l.split(";")))
                 .collect(groupingBy(m -> m.station(), collector)));
 
